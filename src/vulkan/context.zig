18e4b0802ce6980371e50d16b17b83aa7c270e21
theafky <102458483+TheAfky@users.noreply.github.com>
2026-02-16 14:53:48 +0100

    Framebuffers

--- src/vulkan/context.zig
+++ src/vulkan/context.zig
@@ -23,6 +23,7 @@ pub const VulkanContext = struct {
     surface: vk.SurfaceKHR,
     swapchain: Swapchain,
     graphics_pipeline: GraphicsPipeline,
+    framebuffers: []vk.Framebuffer,
 
     pub fn init(allocator: std.mem.Allocator, application_name: [*:0]const u8, engine_name: [*:0]const u8, window: Window) !Self {
         var self: Self = undefined;
@@ -35,16 +36,45 @@ pub const VulkanContext = struct {
         self.device = try Device.init(self.allocator, self.base_wrapper, self.instance.handle, self.surface);
         self.swapchain = try Swapchain.init(self.allocator, self.instance.handle, self.device, self.surface, window);
         self.graphics_pipeline = try GraphicsPipeline.init(self.device, self.swapchain);
+        self.framebuffers = try createFramebuffers(self);
 
         return self;
     }
 
     pub fn deinit(self: Self) void {
+        self.destroyFramebuffers();
         self.graphics_pipeline.deinit();
         self.swapchain.deinit();
         self.device.deinit();
         self.instance.handle.destroySurfaceKHR(self.surface, null);
         self.instance.deinit();
+    }
+
+    fn createFramebuffers(self: Self) ![]vk.Framebuffer {
+        const framebuffers = try self.allocator.alloc(vk.Framebuffer, self.swapchain.swapchain_images.len);
+        errdefer self.allocator.free(framebuffers);
+
+        var i: usize = 0;
+        errdefer for (framebuffers[0..i]) |fb| self.device.handle.destroyFramebuffer(fb, null);
+
+        for (framebuffers) |*framebuffer| {
+            framebuffer.* = try self.device.handle.createFramebuffer(&.{
+                .render_pass = self.graphics_pipeline.render_pass,
+                .attachment_count = 1,
+                .p_attachments = @ptrCast(&self.swapchain.swapchain_images[i].image_view),
+                .width = self.swapchain.extent.width,
+                .height = self.swapchain.extent.height,
+                .layers = 1,
+            }, null);
+            i += 1;
+        }
+
+        return framebuffers;
+    }
+
+    fn destroyFramebuffers(self: Self) void {
+        for (self.framebuffers) |framebuffer| self.device.handle.destroyFramebuffer(framebuffer, null);
+        self.allocator.free(self.framebuffers);
     }
 };
 
